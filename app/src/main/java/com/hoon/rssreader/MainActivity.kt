81bd9c071388c3d2faeb2b69e729de3cbbb68dfa
glshlee <glshlee@gmail.com>
2021-03-05 23:45:42 +0900

    add single thread context

--- app/src/main/java/com/hoon/rssreader/MainActivity.kt
+++ app/src/main/java/com/hoon/rssreader/MainActivity.kt
@@ -2,8 +2,12 @@ package com.hoon.rssreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+
+    val netDispatcher = newSingleThreadContext(name = "ServiceCall")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
