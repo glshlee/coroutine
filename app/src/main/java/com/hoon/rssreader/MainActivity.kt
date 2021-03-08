e1b921e5a232d477d122edf9a87dba280b3f9858
glshlee <glshlee@gmail.com>
2021-03-08 19:06:23 +0900

    add coroutine launch

--- app/src/main/java/com/hoon/rssreader/MainActivity.kt
+++ app/src/main/java/com/hoon/rssreader/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hoon.rssreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(netDispatcher) {
+            // TODO Call coroutine here
+        }
     }
 }
