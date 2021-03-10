a932113d1b8b988d1a563012d100dc23a74f62be
glshlee <glshlee@gmail.com>
2021-03-10 23:42:38 +0900

    add fetch rss headlines

--- app/src/main/java/com/hoon/rssreader/MainActivity.kt
+++ app/src/main/java/com/hoon/rssreader/MainActivity.kt
@@ -5,17 +5,36 @@ import android.os.Bundle
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
 
     private val netDispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val factory = DocumentBuilderFactory.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         GlobalScope.launch(netDispatcher) {
-            // TODO Call coroutine here
+            val headlines = fetchRssHeadlines()
         }
+    }
+
+    private fun fetchRssHeadlines(): List<String> {
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val news = xml.getElementsByTagName("channel").item(0)
+        return (0 until news.childNodes.length)
+                .map { news.childNodes.item(it) }
+                .filter { Node.ELEMENT_NODE == it.nodeType }
+                .map { it as Element }
+                .filter { "item" == it.tagName }
+                .map {
+                    it.getElementsByTagName("title").item(0).textContent
+                }
+
     }
 }
